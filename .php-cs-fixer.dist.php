60ab5d2ff8c15157f4fa900f8ead0a1573f6e2a7
Simon André <smn.andre@gmail.com>
2024-03-23 04:31:57 +0100

    Add licence header

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,12 +1,24 @@
 <?php
 
+$licence = <<<'EOF'
+This file is part of the PhpColor package.
+
+(c) Simon Andre <smn.andre@gmail.com>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+EOF;
+
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
 ;
 
 return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
     ->setRules([
         '@PSR12' => true,
+        'declare_strict_types' => true,
+        'header_comment' => ['header' => $licence],
     ])
     ->setFinder($finder)
 ;
--- tests/ColorTest.php
+++ tests/ColorTest.php
@@ -1,5 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the PhpColor package.
+ *
+ * (c) Simon Andre <smn.andre@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PhpColor\Tests;
 
 use PhpColor\Color;
