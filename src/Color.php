e301af166d0543d9322f2811346990cdd977db97
Simon André <smn.andre@gmail.com>
2026-01-29 13:11:29 +0100

    CS

--- src/Color.php
+++ src/Color.php
@@ -3,12 +3,12 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the PhpColor package.
+ * This file is part of the PHPColor library.
  *
- * (c) Simon André & Raphaêl Geffroy
+ * (c) 2025-present Simon André & Raphaêl Geffroy
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
  */
 
 namespace PhpColor;
